597f55aba7a0b5073089c363166a11b255c5c962
Jonas Pardeyke <jonas@pardeyke.com>
2026-03-22 19:52:09 +0100

    init XCode project

---
+++ AuphonicApp/AuphonicApp/AuphonicAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuphonicAppApp.swift
+//  AuphonicApp
+//
+//  Created by Jonas Pardeyke on 22.03.26.
+//
+
+import SwiftUI
+
+@main
+struct AuphonicAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
